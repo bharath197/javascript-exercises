eee56aa8a55d615e4a6224420749e42003a13530
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-03 01:20:29 +0530

    test case passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a<0||b<0){
+        return 'ERROR'
+    }
+    if(!Number.isInteger(a)||!Number.isInteger(b)){
+        return 'ERROR'
+    }
+    if(b<a){
+        return sumAll(b, a)
+    }
+    let sum= a
+    for(let i = a+1; i<=b;i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
