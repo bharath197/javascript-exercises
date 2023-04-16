92fa53d1d8fb8714d9c584eb9b8b6ab3a2df80c7
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-16 06:06:56 +0530

    test case pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a<=0) return 'OOPS'
+    if(a==1||a==2) return 1
+    for(let i = 1; i<=a; i++){
+        return fibonacci(a-1) + fibonacci(a-2)
+    }
 };
 
 // Do not edit below this line
