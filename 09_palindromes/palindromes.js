9f70a0764ea69c06a108224bd5268a4f6562bbeb
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-16 05:54:54 +0530

    pass test case

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    for(let a = 0, b = s.length-1; a<s.length,b>=0; a++, b--){
+       while(s[a].toLowerCase()==s[a].toUpperCase()) a++
+       while(s[b].toLowerCase()==s[b].toUpperCase()) b--
+       if(s[a].toLowerCase()!=s[b].toLowerCase()) return false
+    }
+    return true
 };
 
 // Do not edit below this line
